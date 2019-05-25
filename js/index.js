ad8592f2d7b8bde068536be143cb39d8fa240edf
jtrill08 <49694135+jtrill08@users.noreply.github.com>
2019-05-25 17:20:34 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,23 @@
-function newQuote() {
-	var randomNumber = Math.floor(Math.random() * (quotes.length));
- 	document.getElementById('quoteDisplay').innerHTML = quotes[randomNumber];
-}
+/**
+ * Javascript Drum Machine
+ * @author Sebastian Inman
+ * @version 1.0.0
+ * @desc This is a javascript experiment created
+ * to demonstrate music patterns in the web browser.
+ * I started this project just for fun and intend on
+ * adding more to it and keeping this pen updated.
+ * All drum samples were provided by 99Samples: 
+ * http://99sounds.org/drum-samples/
+ */
+
+(function(DrummerJS, $, undefined) {
+
+    'use strict';
+
+    // ======================================
+    // establish public variables and methods
+    // ======================================
+
+    DrummerJS.title = 'Javascript Drum Machine';
+    DrummerJS.description = 'Create audio loops using drum  samples.'
+    DrummerJS.author = 'Sebastian Inman';
